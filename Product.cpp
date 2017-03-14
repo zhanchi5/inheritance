1d494102cbe08c6ad24157bea0cce3fcf4e9bd69
zhanchi5 <zhanchi5@outlook.com>
2017-03-14 21:24:40 +0300

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,33 +1,33 @@
 #include "stdafx.h"
 
-Product::Product() // конструктор
+Product::Product() 
 	: price(0.0), weight(0.0)
 {
 	for (unsigned int counter = 0; counter < 50; ++counter)
 		name[counter] = '\0';
 }
 
-void Product::setName(const char *name_product) // установить имя товара
+void Product::setName(const char *name_product)
 {
-	strcpy_s(name, name_product); // имя товара копируем в элемент данных класса
+	strcpy_s(name, name_product); 
 }
 
-const char *Product::getName() const // возвратить имя товара
+const char *Product::getName() const 
 {
 	return name;
 }
 
-void Product::setPrice(const double & pr) // установить стоимость 1 ед. товара
+void Product::setPrice(const double & pr) 
 {
 	price = (pr >= 0 ? pr : 0);
 }
 
-double Product::getPrice() const // возвратить стоимость 1 ед. товара
+double Product::getPrice() const 
 {
 	return price;
 }
 
-void Product::setWeight(const double & wgh) // установить вес товара
+void Product::setWeight(const double & wgh) 
 {
 	weight = wgh;
 }
